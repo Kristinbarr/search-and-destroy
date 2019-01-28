3602601eaaf78a150d2b3c2738dbee728df9c7b2
Kristin Barr <kristinbarr@gmail.com>
2019-01-28 14:46:36 -0500

    BST start

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,7 +2,44 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
+	// base case: if arr.length === 1
+		// if the num is target, return true
+		// else return false
+	// find out root/ middle index by splitting the array in half
+	// if root is greater than the target, 
+	// recurse with the left slice array
 	
+	if (array.length === 0) {
+		// if (array[0] === target) {
+		// 		console.log('true')
+		// 		return true
+		// 	} else {
+		// 			console.log('false')
+		// 			return false
+		// 		}
+				
+				return []
+			}
+			
+	let rootIdx = Math.floor(array.length / 2)
+	let root = array[rootIdx]
+
+	// console.log(array[Math.floor(array.length / 2)], array[Math.ceil(array.length / 2)])
+	console.log(Math.floor(array.length / 2), Math.ceil(array.length / 2))
+	console.log('root', root)
+	
+	if (root > target) {
+		binarySearch(array.slice(0, rootIdx), target)
+	} else {
+		binarySearch(array.slice(rootIdx+1, array.length), target)
+	}
+	
+	if (root === target) {
+		return true
+	} else {
+		return false
+	}
+
 };
 
 /*
